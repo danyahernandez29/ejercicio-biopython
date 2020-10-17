52dd071e652ffeef91c68c0b9da7aef2d56c5dd7
danyahernandez29 <72842412+danyahernandez29@users.noreply.github.com>
2020-10-16 22:14:37 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,15 +1,26 @@
-from bio.seq import seq
-from bio.seqFeature import seqFeature, FeatureLocation
-from Bio import seqRecord
-def summarize_contents(filename):
-        a = SeqIO.read(filename, "genbank")
-        print("Name: ", a.name)
-        import os
-        print ("Path: ", os.path.dirname(filename))
-        b = list(SeqIO.parse(filename, "genbank"))
-        print("num_records = %i records" % len(b))
+from Bio import SeqI0
+from Bio.SeqRecord import SeqRecord
+import os
 
-        for seq_a in SeqIO.parse(filename,"genbank"):
-                print("ID:",a.id)
-         
-summarize_contents(filename)
+# Archivo .gbk de mi escritorio, cambiar a la dirección del nuevo archivo .gbk a leer
+filename = "/mnt/c/Users/danyarenee29/Desktop/Escritorio/comp/genome_assemblies_genome_gbk"
+
+# Definición de la función summarize_contents
+def summarize_contents(filename):
+        lRuta = []
+        lRuta = os.path.split(filename)
+        print("file:", lRuta[1], "\npath:", lRuta[0]
+        all_records=[]
+        records = list(SeqI0.parse(filename, "genbank"))
+        #print ("path: ", os.path.dirname(filename))
+        print("num_records = %i records" % len(records))
+        print("records:")
+        for seq_record in SeqI0.parse(filename, "genbank"):
+              all_records.append(seq_record.name)
+              print("- id:",seq_record.id)
+              print("name ",seq_record.name)
+              print("description: ", seq_record.description)
+              print("\n")
+     
+" Llamada a la función summarize_contents
+  summarize_contents(filename)         
