754c117dd7e5bf915e5ca094a55e8c19f3c3fd4b
danyahernandez29 <72842412+danyahernandez29@users.noreply.github.com>
2020-10-27 19:51:14 -0700

    Update script.py

--- script.py
+++ script.py
@@ -7,28 +7,28 @@ filename = os.path.abspath("data/ls_orchid.gbk")
 
 #Creacion de funcion
 def summarize_contents(filename):
-	listaOs = os.path.split(filename)
-	listaExt = os.path.splitext(filename)
-	if (listaExt[1] == ".gbk"):
+	rptOs = os.path.split(filename)
+	rptSld = os.path.splitext(filename)
+	if (rptSld[1] == ".gbk"):
 		type_file= "genbank"
 	else: 
 		type_file= "fasta"
 	record = list(SeqIO.parse(filename, type_file))
 	#Creacion de diccionario
-	d = {}
-	d['File:'] = listaOs[1]
-	d['Path:'] = listaOs[0]
-	d['Num_records:'] = len(record)
+	di = {}
+	di['File:'] = rptOs[1]
+	di['Path:'] = rptOs[0]
+	di['Num_records:'] = len(record)
 	#Diccionario con listas
-	d['Names:'] = []
-	d['IDs:'] = []
-	d['Descriptions'] = []
+	di['Names:'] = []
+	di['IDs:'] = []
+	di['Descriptions'] = []
 	#Registro de records
 	for seq_rcd in SeqIO.parse(filename,type_file):
-		d['Names:'].append(seq_rcd.name)
-		d['IDs:'].append(seq_rcd.id)
-		d['Descriptions'].append(seq_rcd.description)
-	return d
+		di['Names:'].append(seq_rcd.name)
+		di['IDs:'].append(seq_rcd.id)
+		di['Descriptions'].append(seq_rcd.description)
+	return di
 #Imprimir la funcion
 if __name__ == "__main__":
 	resultados = summarize_contents(filename)
